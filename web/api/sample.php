ba81c70a8cc5b5d3932fee973850b25b04dc88ea
yan <ywang@canamsolutionsinc.com>
2015-08-17 17:52:42 -0400

    add sort in sample.php

--- web/api/sample.php
+++ web/api/sample.php
@@ -82,6 +82,7 @@ function search($term, $location) {
     $url_params['term'] = $term ?: $GLOBALS['DEFAULT_TERM'];
     $url_params['location'] = $location?: $GLOBALS['DEFAULT_LOCATION'];
     $url_params['limit'] = $GLOBALS['SEARCH_LIMIT'];
+	$url_params['sort'] = 1;
     $search_path = $GLOBALS['SEARCH_PATH'] . "?" . http_build_query($url_params);
     
     return request($GLOBALS['API_HOST'], $search_path);
